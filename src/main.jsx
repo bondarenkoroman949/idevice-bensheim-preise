7f08ee77a1de521cc95aca8c046b2fc2b307f2e5
bondarenkoroman949 <bondarenkoroman949@gmail.com>
2025-07-17 12:46:41 +0200

    удалил main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,1 @@
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import PriceList from "./PriceList";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <PriceList />
-  </React.StrictMode>
-);
+// удалено
